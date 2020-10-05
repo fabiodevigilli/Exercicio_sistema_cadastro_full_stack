82d27a75c945ef32ecae8587fb9162deb2eb9787
Fabio Devigilli <fabio.devigilli@hbsis.com.br>
2020-10-05 16:50:06 -0300

    criando solucao e primeiro conexto com healthcheck 05102020.

---
+++ src/FirstOne.Cadastros.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstOne.Cadastros.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
