827e36644536c2911bc167659a0b681f9c2983fc
Fabio Devigilli <fabio.devigilli@hbsis.com.br>
2020-11-03 16:32:34 -0300

    criando folders menu, login e footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/layout/layout.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+    declarations: [
+        MenuComponent,
+        FooterComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        MenuComponent,
+        FooterComponent
+    ]
+})
+export class LayoutModule { }
